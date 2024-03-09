b705bc9a50d48cc21fc5d4ed6bdaafc188cd00d0
Jauvas <143481214+Jauvas@users.noreply.github.com>
2024-03-09 16:36:11 +0300

    n

--- logins/login.js
+++ logins/login.js
@@ -10,31 +10,35 @@ const accounts = document.querySelector('.account-button');
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup')
-})
+});
 
 accounts.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup')
-})
+});
 
 btnPopupRemove.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup')
-})
+});
+
+function remove() {
+    wrapper.classList.remove('active-popup')
+};
 
 function openPopup() {
     wrapper.display = 'block'
-}
+};
 
 function closePopup () {
     wrapper.display = 'none'
-}
+};
 
 function toRegister() {
     login.style.display = 'none'
     register.style.display = 'block'
-}
+};
 
 function toLogin() {
     register.style.display = 'none'
     login.style.display = 'block'
-}
+};
 
